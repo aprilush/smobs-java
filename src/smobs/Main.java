7861ecdaac919c7e009fd1118a8496bb652decba
Laura Dragan <aprilush@gmail.com>
2014-07-29 15:32:10 +0100

    read file

--- src/smobs/Main.java
+++ src/smobs/Main.java
@@ -1,5 +1,9 @@
 package smobs;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,19 +11,45 @@ import java.util.Map;
 public class Main {
 
 	public static void main(String[] args) {
-		Map<String, String> data = readData(args[1]);
+		Map<String, String> data = readData("socms.csv");
 		
 		for (String uri : data.keySet()) {
-			
+			System.out.println(uri+"  -  "+ data.get(uri));
+			SocialMachinePage stub = new SocialMachinePage(data.get(uri), "http://"+uri);
+			stub.generateStubText();			
 		}
-		SocialMachinePage stub = new SocialMachinePage("Anonymous", "http://example.org/");
-		stub.generateStubText();
+//		SocialMachinePage stub = new SocialMachinePage("Anonymous", "http://example.org/");
+//		stub.generateStubText();
 	}
 	
 	/*
 	 * uri - name pairs
 	 */
 	private static Map<String, String> readData(String inputFile) {
-		return new HashMap<String, String>();
+		Map<String, String> lines = new HashMap<String, String>();
+		
+		BufferedReader br = null;
+		String line;
+		String splitBy = ",";
+		try {
+			br = new BufferedReader(new FileReader(inputFile));
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split(splitBy);	 
+				lines.put(parts[0], parts[1]);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return lines;
 	}
 }
